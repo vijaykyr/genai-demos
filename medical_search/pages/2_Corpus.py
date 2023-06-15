2168b2cb30eea95364a8b45781f068f3b0bc76e3
reddyv <reddyv@google.com>
2023-06-15 16:52:28 -0400

    Better page layout

--- medical_search/pages/2_Corpus.py
+++ medical_search/pages/2_Corpus.py
@@ -8,19 +8,26 @@ st.set_page_config(
     page_icon='app/images/logo.png',
 )
 
-st.markdown('### Select a document to preview')
-col1, col2 = st.columns([1,3])
+st.markdown('### Select a paper to browse')
+st.markdown('''
+    - These are all the papers uploaded to Enterprise Search for this demo. 
+    - Given a query Enterprise Search will identify which papers in its corpus 
+    contain relevant information.''')
 df = pd.read_json("data/articles.json")
 gb = GridOptionsBuilder.from_dataframe(df[['title']])
-gb.configure_selection(selection_mode="single")
+gb.configure_selection()
+gb.configure_column('title', header_name="Paper Title")
+gb.configure_pagination()
 gridOptions = gb.build()
 
-with col1:
-    data = AgGrid(df, gridOptions=gridOptions)
+
+data = AgGrid(
+    df,
+    gridOptions=gridOptions,
+    fit_columns_on_grid_load=True)
 
 selected_rows = data["selected_rows"]
 
-with col2:
-    if len(selected_rows) != 0:
-        st.markdown(f"*NCBI REF:* {selected_rows[0]['ncbi_ref']}")
-        st.markdown(utils.show_pdf(selected_rows[0]['download']), unsafe_allow_html=True)
+if len(selected_rows) != 0:
+    st.markdown(f"*NCBI REF:* {selected_rows[0]['ncbi_ref']}")
+    st.markdown(utils.show_pdf(selected_rows[0]['download']), unsafe_allow_html=True)
