17ea12b2620ee07b2bb645223510fc4f92d84116
reddyv <reddyv@google.com>
2023-06-16 11:32:42 -0400

    Update retriever class. Fix bug in fetching source metadata

--- medical_search/retrieval.py
+++ medical_search/retrieval.py
@@ -35,7 +35,107 @@ from pydantic import BaseModel, Extra, root_validator, Field
 
 import utils
 
+class EnterpriseSearchRetriever(BaseRetriever, BaseModel):
+    """Wrapper around Google Cloud Enterprise Search."""
+    client: Any = None #: :meta private:
+    serving_config: Any = None #: :meta private:Any
+    content_search_spec: Any = None #: :meta private:Any
+    project_id: str = None
+    search_engine_id: str = None
+    serving_config_id: str = 'default_config'
+    location_id: str = 'global'
+    max_snippet_count: int = 3
+    max_matches: int = 15
+    query_expansion_condition: int = 1
+    credentials: Any = None
+    "The default custom credentials (google.auth.credentials.Credentials) to use "
+    "when making API calls. If not provided, credentials will be ascertained from "
+    "the environment."
 
+
+    class Config:
+        """Configuration for this pydantic object."""
+        extra = Extra.forbid
+        arbitrary_types_allowed = True
+
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        try:
+            from google.cloud import discoveryengine_v1beta
+        except ImportError:
+            raise ImportError(
+                "google.cloud.discoveryengine is not installed. "
+                "Please install it with pip install google-cloud-discoveryengine"
+            )
+
+        values["project_id"] = get_from_dict_or_env(values, "project_id", "PROJECT_ID")
+        values["search_engine_id"] = get_from_dict_or_env(values, "search_engine_id", "SEARCH_ENGINE_ID")
+        values["location_id"] = get_from_dict_or_env(values, "location_id", "LOCATION_ID")
+        values["max_snippet_count"] = get_from_dict_or_env(values, "max_snippet_count", "MAX_SNIPPET_COUNT")
+        values["query_expansion_condition"] = get_from_dict_or_env(values, "query_expansion_condition", "QUERY_EXPANSION_CONDITION")
+
+        client = SearchServiceClient(credentials=values['credentials'])
+        values["client"] = client
+
+        values["serving_config"] = client.serving_config_path(
+            project=values["project_id"],
+            location=values["location_id"],
+            data_store=values["search_engine_id"],
+            serving_config=values['serving_config_id'],
+        )
+
+        values["content_search_spec"] = {
+            'snippet_spec': {
+                'max_snippet_count': values["max_snippet_count"],
+            }
+        }
+        values["query_expansion_spec"] = {
+            'condition': values["query_expansion_condition"],
+        }
+
+        return values
+
+    def _convert_search_response(self, search_results):
+        """Converts search response to a list of LangChain documents."""
+        documents = []
+        for result in search_results:
+            doc_info = MessageToDict(result.document._pb)
+            if doc_info.get('derivedStructData'):
+                if doc_info.get('structData'):
+                    metadata = doc_info.get('structData')
+                else:
+                    metadata = {}
+                for snippet in doc_info.get('derivedStructData', {}).get('snippets', []):
+                    if snippet.get('snippet') is not None:
+                        metadata['source'] = f"{doc_info.get('derivedStructData').get('link')}:{snippet.get('pageNumber')}"
+                        metadata['id'] = doc_info.get('id')
+                        document = Document(
+                            page_content=snippet.get('snippet'),
+                            metadata=metadata
+                        )
+                        documents.append(document)
+
+        return documents
+
+    def get_relevant_documents(self, query: str) -> List[Document]:
+        """Get documents relevant for a query."""
+
+        request = SearchRequest(
+            query=query,
+            serving_config=self.serving_config,
+            content_search_spec=self.content_search_spec,
+            query_expansion_spec=self.query_expansion_spec,
+        )
+        response = self.client.search(request)
+        documents = self._convert_search_response(response.results)
+
+        if len(documents) > self.max_matches:
+            documents = documents[0:self.max_matches]
+
+        return documents
+
+    async def aget_relevant_documents(self, query: str) -> List[Document]:
+        raise NotImplementedError("Async interface to GDELT not implemented")
 #############
 # This is a temporary (bad) hack for the prototype. We will need a better way - hopefully using ES metatada
 # to get the article name
@@ -48,89 +148,12 @@ def _get_sources(document_list: List[str]) -> List[str]:
     for doc in document_list:
         file_name = doc.split('/')[-1]
         for article in _articles:
-            if file_name == article['download'].split('/')[-1]:
+            if file_name.startswith(article['download'].split('/')[-1]):
                 source = f'[{article["title"]}]({article["ncbi_ref"]})'
                 sources.append(source)
 
     return sources
 ############
-
-
-class EnterpriseSearchRetriever(BaseRetriever, BaseModel):
-    client: Any
-    serving_config: Any
-    project_id: Optional[str] = None
-    search_engine_id: Optional[str] = None
-    serving_config_id: str = 'default_config'
-    location_id: str = 'global'
-    max_snippet_count: int = 5
-
-    class Config:
-        """Configuration for this pydantic object."""
-        extra = Extra.forbid
-
-    @root_validator()
-    def validate_environment(cls, values: Dict) -> Dict:
-        project_id = get_from_dict_or_env(values, "project_id", "PROJECT_ID")
-        values["project_id"] = project_id
-        search_engine_id = get_from_dict_or_env(values, "search_engine_id", "SEARCH_ENGINE_ID")
-        values["search_engine_id"] = search_engine_id
-        location_id = get_from_dict_or_env(values, "location_id", "LOCATION_ID")
-        values["location_id"] = location_id
-        max_snippet_count = get_from_dict_or_env(values, "max_snippet_count", "MAX_SNIPPET_COUNT")
-        values["max_snippet_count"] = max_snippet_count
-
-        client = SearchServiceClient()
-        values["client"] = client
-        serving_config = client.serving_config_path(
-            project=project_id,
-            location=location_id,
-            data_store=search_engine_id,
-            serving_config=values['serving_config_id'],
-        )
-        values["serving_config"] = serving_config
-        return values
-
-    def _search(self, query:str):
-        content_search_spec = {
-            'snippet_spec': {
-                'max_snippet_count': 5
-            }
-        }
-        request = SearchRequest(
-            serving_config=self.serving_config,
-            query=query,
-            content_search_spec=content_search_spec,
-        )
-        response = self.client.search(request)
-        return response.results
-
-    def _convert_search_response(self, search_results):
-        documents = []
-        for result in search_results:
-            doc_info = MessageToDict(result.document._pb)
-            if doc_info.get('derivedStructData'):
-                content = [snippet.get('snippet') for snippet in doc_info.get('derivedStructData', {}).get('snippets', []) if snippet.get('snippet') is not None]
-                if content:
-                    document = Document(
-                        page_content='\n'.join(content),
-                        metadata={
-                            'source': doc_info.get('derivedStructData').get('link'),
-                            'id': doc_info.get('derivedStructData').get('id')
-                        }
-                    )
-                    documents.append(document)
-
-        return documents
-
-    def get_relevant_documents(self, query: str) -> List[Document]:
-        results = self._search(query)
-        documents = self._convert_search_response(results)
-        return documents
-
-    async def aget_relevant_documents(self, query: str) -> List[Document]:
-        raise NotImplementedError("Async interface to GDELT not implemented")
-
 
 def create_retrieval_chain() -> RetrievalQAWithSourcesChain:
     retriever = EnterpriseSearchRetriever(
