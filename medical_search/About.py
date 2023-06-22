fc7b0b613d6437485595faeeb7da0a85ac6cdd43
reddyv <reddyv@google.com>
2023-06-22 11:38:05 -0400

    update about page

--- medical_search/About.py
+++ medical_search/About.py
@@ -10,11 +10,23 @@ with cols[0]:
     st.image('app/images/logo.png')
 with cols[1]:
     st.title('Q&A over Biomedical Literature')
-
-st.write('''Demo using BioASQ dataset''')
-
 st.image('app/images/architecture.png')
-
-
-st.subheader('How it Works')
-st.write('Description here...')
+st.markdown('''
+    ### About
+    This demo illustrates the use of Gen App Builder Enterprise Search to perform document retrieval and question 
+    answering with citation on a biomedical corpus. It allows researches to quickly verify generated answers 
+    via the presentation of relevant source materials.
+    
+    ### Enterprise Search
+    The use of Enterprise Search provides powerful functionality out of the box:
+    1. It not only fetches the most relevant documents, it also identifies the most relevant snippets within each 
+    document.
+    2. It feeds the relevant snippets into an LLM and generates a summary answer. In effect it is combining 
+    both extractive document retrieval with generative summarization. 
+    
+    ### Dataset
+    The questions in this demo were sourced from the [BioASQ Challenge](http://bioasq.org/) task B. This consists of 
+    a benchmark datasets containing biomedical questions along with gold standard (reference) answers constructed by a 
+    team of biomedical experts. The benchmark dataset also provides a list of relevant articles that support the answer. 
+    We take a subset of these questions and relevant articles to form the Enterprise Search corpus.
+''')
